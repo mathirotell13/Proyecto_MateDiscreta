7014a2f0ce1d40f06c1f9d54d102e0b34662ead0
Mathías Rotell <162376412+mathirotell13@users.noreply.github.com>
2025-06-22 16:42:36 -0500

    Update semana2_induccion_completa_recurrencia.cpp

---
+++ Unidad_1/semana2_induccion_completa_recurrencia.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+void Generar_EcRecurrencia(int grado) {
+    if (grado != 2 && grado != 3) {
+        cout<<"ERROR: El ejercicio te pide solo de grado 2 o 3.\n";
+        return;
+    }
+
+    double r[3], A[3]; //para guardar raíces y los coeficientes A,B,C
+
+    for (int i = 0; i < grado; i++)
+    {   cout<<"Ingrese r"<<(i + 1)<<": ";
+        cin>>r[i]; }
+
+    for (int i = 0; i < grado; i++)
+    {   cout<<"Ingrese "<<char(65+i)<<": ";
+        cin>>A[i];  }
+
+    //Verificar raíces
+    for (int i = 0; i < grado; i++)
+    {   for (int k = i+1; k < grado; k++)
+        {   if (r[k]==r[i])
+            {   cout<<"Error: Las raices deben ser distintas.\n";
+                return;   }
+        }
+    }
+
+    // Mostrar ecuación de recurrencia
+    cout<<"\nLa ecuación en recurrencia que satisface a_n es:\n";
+
+    if (grado == 2)
+    {   double a = -(r[0] + r[1]);
+        double b = r[0] * r[1];
+        cout<<"a_n + ("<<a<<")a_{n-1} + ("<<b<<")a_{n-2} = 0\n";
+    }
+    else if (grado == 3)
+    {
+        double a = -(r[0] + r[1] + r[2]);
+        double b = r[0]*r[1] + r[0]*r[2] + r[1]*r[2];
+        double c = -r[0]*r[1]*r[2];
+        cout << "a_n + (" << a << ")a_{n-1} + (" << b << ")a_{n-2} + (" << c << ")a_{n-3} = 0\n";
+    }
+
+  //NOTA ADICIONAL: luego le agrego la opción para ver las condiciones iniciales a_0, a_1, a_2
+
+}
+
+int main() {
+    int grado;
+    cout<<"Ingresa el grado de la solucion: ";
+    cin>>grado;
+
+    Generar_EcRecurrencia(grado);
+    return 0;
+}
